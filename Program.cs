cd5f5d8a337d15461ebf13137815cc025e28e849
Eslam <Eslam@DESKTOP-UVNEOHL>
2024-12-30 23:56:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+// connect to redis
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+IDatabase database = redis.GetDatabase();
+
+setGetStringRedis(database);
+
+void setGetStringRedis(IDatabase db)
+{
+    db.StringSet("saber:key", "saber:value");
+    Console.WriteLine(db.StringGet("saber:key"));
+}
