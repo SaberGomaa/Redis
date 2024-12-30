efda841d82939094200ca986cb3bb8c9ebc12728
SaberGomaa <sabergomaa261@gmail.com>
2024-12-31 00:27:22 +0200

    Switch Redis interaction to use hash instead of string
    Commented out setGetStringRedis call and introduced setGetHashRedis
    to demonstrate working with Redis hashes. The new method sets
    multiple key-value pairs in a Redis hash and retrieves and prints
    all entries in the hash.

--- Program.cs
+++ Program.cs
@@ -4,10 +4,27 @@
 ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
 IDatabase database = redis.GetDatabase();
 
-setGetStringRedis(database);
+//setGetStringRedis(database);
+setGetHashRedis(database);
 
 void setGetStringRedis(IDatabase db)
 {
     db.StringSet("saber:key", "saber:value");
     Console.WriteLine(db.StringGet("saber:key"));
+}
+
+void setGetHashRedis(IDatabase db)
+{
+    db.HashSet("saber:hash", new HashEntry[]
+    {
+        new HashEntry("key1", "value1"),
+        new HashEntry("key2", "value2")
+    });
+
+
+    HashEntry[] hashEntries = db.HashGetAll("saber:hash");
+    foreach (var entry in hashEntries)
+    {
+        Console.WriteLine($"{entry.Name}: {entry.Value}");
+    }
 }
